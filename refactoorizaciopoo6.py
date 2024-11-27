eb7f23f09449c09ed62c777d938a97cfa0d04a94
yeisonpabon <yeison.villegas@correounivalle.edu.co>
2024-11-26 23:32:35 -0500

    Update refactoorizaciopoo6.py

--- refactoorizaciopoo6.py
+++ refactoorizaciopoo6.py
@@ -7,16 +7,12 @@ class Vehiculo:
         tiempo = distancia / self.velocidad
         return f"Tiempo de viaje en {self.nombre}: {tiempo:.2f} horas"
 
-
-# Instancias específicas de vehículos
-coche = Vehiculo(velocidad=80, nombre="coche")
+carro = Vehiculo(velocidad=80, nombre="carro")
 bicicleta = Vehiculo(velocidad=15, nombre="bicicleta")
-autobus = Vehiculo(velocidad=20, nombre="autobús")
+motocicleta = Vehiculo(velocidad=20, nombre="motocicleta")
 
-# Lista de vehículos
-vehiculos = [coche, bicicleta, autobus]
+vehiculos = [carro, bicicleta, motocicleta]
 
-# Cálculo de tiempos de viaje
 distancia = 1000
 for vehiculo in vehiculos:
     print(vehiculo.calcular_tiempo_viaje(distancia))
